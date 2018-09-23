8e00186d218320a680912727c6fa9a81d796963a
hezizi <yucihent@sina.com>
2018-09-23 13:43:58 +0800

    ipdate

--- request和response/req.js
+++ request和response/req.js
@@ -10,16 +10,16 @@ http.createServer((req, res) => {
   // resquest.url
 
   // 1. req.headers返回一个对象，包含所有的请求报文头
-  // console.log(req.headers);
+  console.log(req.headers);
 
   // 2. req.rawHeaders返回一个数组，保存请求报文头的字符串
-  // console.log(req.rawHeaders);
+  console.log(req.rawHeaders);
 
   // 3. resquest.httpVersion获取请求客户端所使用的http版本号
-  // console.log(req.httpVersion);
+  console.log(req.httpVersion);
 
   // 4. resquest.method获取用户请求方式
-  // console.log(req.method);
+  console.log(req.method);
 
   // 5. resquest.url请求报文请求路径（GET），不包含主机名
   console.log(req.url);
--- request和response/res.js
+++ request和response/res.js
@@ -3,17 +3,20 @@ const http = require('http');
 // response （http.ServerResponse 类）
 
 http.createServer((req, res) => {
+  // response.setHeader();
+  // response.statusCode
   // response.write()
   // response.end()
-  // response.setHeader();
-  // response.method
-  // response.url
 
 
   // res.setHeader()
   // 放在res.write()和res.end()之前设置，不设置响应报文头，系统默认有响应报文头，默认发送给浏览器
   // 当已经有响应报文头后就不能在使用res.setHeader()来设置，否则报错
-  res.setHeader('Content-Type', 'text/plain', 'charset=utf-8');
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+
+  // res.statusCode设置响应状态码；res.statusMessage设置响应状态码对应的消息，配合res.setHeader()使用
+  res.statusCode == 404;
+  res.statusMessage = 'Not Found';
 
   // res.write();
   res.write('hello，你好');
@@ -21,7 +24,6 @@ http.createServer((req, res) => {
   // 每个请求都必须调用res.end()
   // 告诉服务器该响应的报文头，报文体等已经全部响应文笔，可以结束响应
   res.end();
-
 
 }).listen('3001', () => {
   console.log('服务已启动');
